b6e143e6e246d5b949472d64b418e7b42955e6e9
sbedene <sylvainbedene@gmail.com>
2019-10-08 16:44:40 +0200

    bug with realpath

--- res/src/write.c
+++ res/src/write.c
@@ -11,7 +11,8 @@ int     ud_file_write_ctr(char *path, ud_arr *content, int flag)
     if (fd == -1)
     {
         char resolved_path[PATH_MAX];
-        realpath(path, resolved_path);
+        if (!realpath(path, resolved_path))
+            ud_ut_error("Couldn't resolve absolute path.");
         ud_ut_error("Couldn't create file %s, check that the provided path exists.", resolved_path);
     }
     char    *t_content_val = (char *)content->val;
