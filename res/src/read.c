befc52fb4f9bceba6448c918ceb0dc004dcc5c71
sbedene <sylvainbedene@gmail.com>
2019-10-21 14:09:09 +0200

    opti file read

--- res/src/read.c
+++ res/src/read.c
@@ -7,29 +7,32 @@ ud_arr  *ud_file_read(char *path)
         return (NULL);
     
     size_t  buf_size = 4096;
-    ud_arr  *buf = ud_arr_tinit(ud_stra_type_char(), buf_size);
-    char    *t_buf_val = (char *)buf->val;
+    char    buf[buf_size];
     size_t  len = 0;
     size_t  total_len = 0;
     ud_list *buf_list = NULL;
     ud_list *curr = NULL;
 
-    while ((len = read(fd, t_buf_val, buf_size)) > 0)
+    while ((len = read(fd, buf, buf_size)) > 0)
     {
-        buf->len = len;
         if ((!buf_list && !(buf_list = ud_list_init(buf))) || (curr && !(curr = ud_list_add_last(curr, buf))))
             return NULL;
         total_len += len;
     }
-    ud_arr *content = ud_arr_tinit(ud_stra_type_char(), total_len + 1);
-    char *t_content_val = (char *)content->val;
+
+    ud_arr  *content = ud_arr_tinit(ud_stra_type_char(), total_len + 1);
+    char    *t_content_val = (char *)content->val;
     ud_list *buf_list_tmp = buf_list;
+    ud_list *tmp_free;
+
     while (buf_list_tmp)
     {
-        ud_arr *list_data = (ud_arr *)buf_list_tmp->data;
-        ud_mem_cpy_rd(t_content_val, list_data->val, list_data->len);
+        char *list_data = (char *)buf_list_tmp->data;
+        ud_mem_cpy_rd(t_content_val, list_data, buf_list_tmp->next ? 4096 : total_len % 4096);
+        tmp_free = buf_list_tmp;
         buf_list_tmp = buf_list_tmp->next;
+        ud_ut_free(tmp_free);
     }
-    ud_list_free(buf_list);
-    return (content);
+    
+    return content;
 }
