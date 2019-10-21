f5f459a77de511a0025862a734d1ae77966c9b94
sbedene <sylvainbedene@gmail.com>
2019-10-21 13:15:49 +0200

    opti list add

--- res/src/read.c
+++ res/src/read.c
@@ -12,11 +12,13 @@ ud_arr  *ud_file_read(char *path)
     size_t  len = 0;
     size_t  total_len = 0;
     ud_list *buf_list = NULL;
+    ud_list *curr = NULL;
 
     while ((len = read(fd, t_buf_val, buf_size)) > 0)
     {
         buf->len = len;
-        buf_list = ud_list_init_or_push(buf_list, buf);
+        if ((!buf_list && !buf_list = ud_list_init(buf)) || (curr && !curr = ud_list_add_last(curr, buf)))
+            return NULL;
         total_len += len;
     }
     ud_arr *content = ud_arr_tinit(ud_stra_type_char(), total_len + 1);
