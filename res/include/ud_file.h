ed8f6c4893a3eb795a7dfa2e14aa6ca4ba1bf065
sbedene <sylvainbedene@gmail.com>
2019-10-22 23:10:15 +0200

    fix in ud file exists

--- res/include/ud_file.h
+++ res/include/ud_file.h
@@ -13,7 +13,7 @@
 // Macro
 # define ud_file_write(path, content)           ud_file_write_ctr(path, content, 0)
 # define ud_file_write_append(path, content)    ud_file_write_ctr(path, content, O_APPEND)
-# define ud_file_exists(_path)                  ({ int _fd = open(_path, O_RDONLY); ud_bool _exists = !_path || access(_path, R_OK) || (read(_fd, NULL, 0) == -1); close(_fd); _exists })
+# define ud_file_exists(_path)                  ({ int _fd = open(_path, O_RDONLY); ud_bool _exists = !_path || access(_path, R_OK) || (read(_fd, NULL, 0) == -1); close(_fd); _exists; })
 # define BUF_SIZE                               4096
 
 // Structures
