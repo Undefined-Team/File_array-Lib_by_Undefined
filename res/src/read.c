60aa34f84cd70b47fb33af4173a8cf3fc5e430df
sbedene <sylvainbedene@gmail.com>
2019-10-22 22:17:45 +0200

    set buf size as macro instead of variable

--- res/src/read.c
+++ res/src/read.c
@@ -6,26 +6,25 @@ ud_arr  *ud_file_read(char *path)
     if (!path || access(path, R_OK) || read(fd, NULL, 0) == -1)
         return (NULL);
     
-    size_t              buf_size = 4096;
-    char                buf[buf_size];
+    char                buf[BUF_SIZE];
     size_t              len = 0;
     size_t              total_len = 0;
 
     typedef struct      uds_file_read_list {
         void            (*fp_free)(void *val);
         struct          uds_file_read_list *next;
-        char            data[buf_size];
+        char            data[BUF_SIZE];
     }                   ud_file_read_list;
 
     ud_file_read_list   *buf_list = NULL;
     ud_file_read_list   *curr = NULL;
 
-    len = read(fd, buf, buf_size);
+    len = read(fd, buf, BUF_SIZE);
     total_len += len;
     buf_list = ud_list_minit(ud_file_read_list);
     ud_mem_cpy(buf_list->data, buf, len);
     curr = buf_list;
-    while ((len = read(fd, buf, buf_size)) > 0)
+    while ((len = read(fd, buf, BUF_SIZE)) > 0)
     {
         curr = ud_list_mpush(curr);
         ud_mem_cpy(curr->data, buf, len);
@@ -37,7 +36,7 @@ ud_arr  *ud_file_read(char *path)
     curr = buf_list;
     while (curr)
     {
-        ud_mem_cpy_rd(t_content_val, (char *)curr->data, curr->next ? buf_size : total_len % buf_size);
+        ud_mem_cpy_rd(t_content_val, (char *)curr->data, curr->next ? BUF_SIZE : total_len % BUF_SIZE);
         curr = curr->next;
     }
     *t_content_val = '\0';
