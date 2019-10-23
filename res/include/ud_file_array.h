b623992e3af373c8f4814736d6594a0e17cf834a
sbedene <sylvainbedene@gmail.com>
2019-10-23 21:28:19 +0200

    update for using file lib

--- res/include/ud_file_array.h
+++ res/include/ud_file_array.h
@@ -8,12 +8,12 @@
 #include <stdlib.h> // for realpath
 #include <limits.h> // for PATH_MAX
 #include <ud_string_array.h>
-#include <ud_list.h>
+#include <ud_file.h>
 
 // Macro
-# define ud_file_array_write(path, content)         ud_file_write_ctr(path, content, 0)
-# define ud_file_array_write_append(path, content)  ud_file_write_ctr(path, content, O_APPEND)
-# define BUF_SIZE                                   4096
+# define ud_file_array_write(_path, _content)           ud_file_write_ctr(_path, (char *)_content->val, 0)
+# define ud_file_array_write_append(_path, _content)    ud_file_write_ctr(_path, (char *)_content->val, O_APPEND)
+# define BUF_SIZE                                       4096
 
 // Structures
 
--- res/src/read.c
+++ res/src/read.c
@@ -2,46 +2,7 @@
 
 ud_arr  *ud_filea_read(char *path)
 {
-    int     fd = open(path, O_RDONLY);
-    if (!path || access(path, R_OK) || read(fd, NULL, 0) == -1)
-        return (NULL);
-    
-    char                buf[BUF_SIZE];
-    size_t              len = 0;
-    size_t              total_len = 0;
-
-    typedef struct      uds_file_read_list {
-        void            (*fp_free)(void *val);
-        struct          uds_file_read_list *next;
-        char            data[BUF_SIZE];
-    }                   ud_file_read_list;
-
-    ud_file_read_list   *buf_list = NULL;
-    ud_file_read_list   *curr = NULL;
-
-    len = read(fd, buf, BUF_SIZE);
-    total_len += len;
-    buf_list = ud_list_minit(ud_file_read_list);
-    ud_mem_cpy(buf_list->data, buf, len);
-    curr = buf_list;
-    while ((len = read(fd, buf, BUF_SIZE)) > 0)
-    {
-        curr = ud_list_mpush(curr);
-        ud_mem_cpy(curr->data, buf, len);
-        total_len += len;
-    }
-
-    ud_arr  *content = ud_arr_tinit(ud_arr_type_char(), total_len + 1);
-    char    *t_content_val = (char *)content->val;
-    curr = buf_list;
-    while (curr)
-    {
-        ud_mem_cpy_rd(t_content_val, (char *)curr->data, curr->next ? BUF_SIZE : total_len % BUF_SIZE);
-        curr = curr->next;
-    }
-    *t_content_val = '\0';
-    ud_list_free(buf_list);
-
-    close(fd);
-    return content;
+    size_t len = 0;
+    char *content = ud_file_read_ctr(path, &len);
+    return ud_arr_tnew(ud_arr_type_char(), len, content);
 }
