7803321e54c1faa5ad3f7fa5b74b21a75b63e954
sbedene <sylvainbedene@gmail.com>
2019-10-24 10:54:23 +0200

    change file_array to filea

--- res/include/ud_file_array.h
+++ res/include/ud_file_array.h
@@ -11,15 +11,15 @@
 #include <ud_file.h>
 
 // Macro
-# define ud_file_array_write(_path, _content)           ud_file_write_ctr(_path, (char *)_content->val, 0)
-# define ud_file_array_write_append(_path, _content)    ud_file_write_ctr(_path, (char *)_content->val, O_APPEND)
+# define ud_filea_write(_path, _content)           ud_file_write_ctr(_path, (char *)_content->val, 0)
+# define ud_filea_write_append(_path, _content)    ud_file_write_ctr(_path, (char *)_content->val, O_APPEND)
 # define BUF_SIZE                                       4096
 
 // Structures
 
 // Prototypes
-ud_arr  *ud_file_array_read(char *path);
-int     ud_file_array_write_ctr(char *path, ud_arr *content, int flag);
-void    ud_file_array_clear(char *path);
+ud_arr  *ud_filea_read(char *path);
+int     ud_filea_write_ctr(char *path, ud_arr *content, int flag);
+void    ud_filea_clear(char *path);
 
 #endif
